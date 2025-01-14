02bc051e8d3387a8f0507bea39d3ee44ce85a477
Thiago Lobato <thiagohgl@hotmail.com>
2025-01-14 20:54:33 +0100

    Forgot function to get phonem converter

--- RuleBasedModels.py
+++ RuleBasedModels.py
@@ -5,6 +5,17 @@ import epitran
 import eng_to_ipa
 
 
+def get_phonem_converter(language: str):
+    if language == 'de':
+        phonem_converter = EpitranPhonemConverter(
+            epitran.Epitran('deu-Latn'))
+    elif language == 'en':
+        phonem_converter = EngPhonemConverter()
+    else:
+        raise ValueError('Language not implemented')
+
+    return phonem_converter
+
 class EpitranPhonemConverter(ModelInterfaces.ITextToPhonemModel):
     word_locations_in_samples = None
     audio_transcript = None
