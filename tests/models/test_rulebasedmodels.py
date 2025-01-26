b5e48988dbebf15ad9a602dbc918644c7efdcc61
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2025-01-26 22:20:29 +0100

    test: add more RuleBasedModels test cases

--- tests/models/test_rulebasedmodels.py
+++ tests/models/test_rulebasedmodels.py
@@ -9,13 +9,28 @@ words_transcribed = 'Ic hab zeh viel guck am und gesund tu sein'
 
 
 class TestPhonemConverter(unittest.TestCase):
+    def test_get_phonem_converter_de(self):
+        converter = RuleBasedModels.get_phonem_converter('de')
+        self.assertIsInstance(converter, RuleBasedModels.EpitranPhonemConverter)
 
-    def test_english_ok(self):
+    def test_get_phonem_converter_en(self):
+        converter = RuleBasedModels.get_phonem_converter('en')
+        self.assertIsInstance(converter, RuleBasedModels.EngPhonemConverter)
+
+    def test_get_phonem_converter_invalid_language(self):
+        with self.assertRaises(ValueError):
+            try:
+                RuleBasedModels.get_phonem_converter('fr')
+            except ValueError as ve:
+                self.assertEqual(str(ve), 'Language not implemented')
+                raise ve
+
+    def test_converttophonem_de(self):
         phonem_converter = RuleBasedModels.EngPhonemConverter()
         output = phonem_converter.convertToPhonem('Hello, this is a test')
         self.assertEqual(output, 'hɛˈloʊ, ðɪs ɪz ə tɛst')
 
-    def test_german_ok(self):
+    def test_converttophonem_en(self):
         deu_latn = epitran.Epitran('deu-Latn')
         phonem_converter = RuleBasedModels.EpitranPhonemConverter(deu_latn)
         output = phonem_converter.convertToPhonem('Hallo, das ist ein Test')
