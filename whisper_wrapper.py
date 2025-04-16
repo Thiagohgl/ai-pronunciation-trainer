5d99359150059d8bd7c89ad698efe52200d0b342
Do Tri Nhan <v.nhandt23@vinbigdata.org>
2025-04-16 21:41:40 +0700

    [Update] Using whisper

--- whisper_wrapper.py
+++ whisper_wrapper.py
@@ -5,18 +5,20 @@ from typing import Union
 import numpy as np 
 
 class WhisperASRModel(IASRModel):
-    def __init__(self, model_name="openai/whisper-base"):
+    def __init__(self, model_name="openai/whisper-large"):
         self.asr = pipeline("automatic-speech-recognition", model=model_name, return_timestamps="word")
         self._transcript = ""
         self._word_locations = []
         self.sample_rate = 16000
 
-    def processAudio(self, audio:Union[np.ndarray, torch.Tensor]):
+    def processAudio(self, audio:Union[np.ndarray, torch.Tensor], lang="en"):
         # 'audio' can be a path to a file or a numpy array of audio samples.
         if isinstance(audio, torch.Tensor):
             audio = audio.detach().cpu().numpy()
-        result = self.asr(audio[0])
+        result = self.asr(audio[0], generate_kwargs={"language": "<|" + lang + "|>"})
         self._transcript = result["text"]
+
+        print("---------------->result: ", result)
         self._word_locations = [{"word":word_info["text"], "start_ts":word_info["timestamp"][0]*self.sample_rate,
                                  "end_ts":word_info["timestamp"][1]*self.sample_rate} for word_info in result["chunks"]]
 
@@ -26,3 +28,14 @@ class WhisperASRModel(IASRModel):
     def getWordLocations(self) -> list:
         
         return self._word_locations
+    
+if __name__ == "__main__":
+    import torchaudio
+    model = WhisperASRModel()
+
+
+    wav, sr = torchaudio.load("short_sample.wav")
+    input = wav[0].numpy()
+    model.processAudio([input])
+    print(model.getTranscript())
+    print(model.getWordLocations())  # This will print the word locations in the audio file.
